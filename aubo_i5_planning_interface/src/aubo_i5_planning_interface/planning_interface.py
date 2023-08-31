41bc5b775916ef2c4557de17be044134a436674e
Ian Chuang <iantc104@gmail.com>
2023-08-31 00:16:36 -0700

    use pilz motion planner

--- aubo_i5_planning_interface/src/aubo_i5_planning_interface/planning_interface.py
+++ aubo_i5_planning_interface/src/aubo_i5_planning_interface/planning_interface.py
@@ -1,5 +1,7 @@
+import rospy
 import moveit_commander
 from geometry_msgs.msg import Pose
+from moveit_msgs.srv import SetPlannerParamsRequest, SetPlannerParams
 from aubo_i5_planning_interface.utils.control_utils import (
     offset_pose,
 )
@@ -15,19 +17,23 @@ class PlanningInterface:
         self.arm = moveit_commander.MoveGroupCommander("arm")
 
         self._position_limits = np.array([
-            [0.2, -0.5, 0.1],
-            [0.6, 0.5, 0.3],
+            [0.1, -0.5, 0.1],
+            [0.6, 0.5, 1],
         ])
         self._orientation_limits = np.array([
             [-1.57, -1.57, -3.14],
             [1.57, 1.57, 3.14],
         ])
 
+        
+
     def move_home(
             self,
             vel_scale=_DEFAULT_VELOCITY_SCALE,
             acc_scale=_DEFAULT_ACCELERATION_SCALE
         ) -> None:
+        self.arm.set_planner_id("PTP")
+
         self.arm.set_max_velocity_scaling_factor(vel_scale)
         self.arm.set_max_acceleration_scaling_factor(acc_scale)
         self.arm.set_named_target("home")
@@ -48,10 +54,7 @@ class PlanningInterface:
             vel_scale=_DEFAULT_VELOCITY_SCALE, 
             acc_scale=_DEFAULT_ACCELERATION_SCALE
         ) -> None:
-
-        self.arm.set_max_velocity_scaling_factor(vel_scale)
-        self.arm.set_max_acceleration_scaling_factor(acc_scale)
-
+        self.set_pipeline("pilz_industrial_motion_planner")
         self.arm.set_planner_id("LIN")
 
         self.arm.set_max_velocity_scaling_factor(vel_scale)
@@ -105,4 +108,17 @@ class PlanningInterface:
 
         self.move_line(target_pose, vel_scale, acc_scale)
 
-    
+    def set_pipeline(self, pipeline_id):
+        # Create a service proxy to call the /set_planner_params service
+        set_planner_params_service = rospy.ServiceProxy('/set_planner_params', SetPlannerParams)
+
+        # Prepare the request data
+        request = SetPlannerParamsRequest()
+        request.pipeline_id = pipeline_id
+        request.replace = False
+
+        # Call the service and handle the response
+        try:
+            set_planner_params_service(request)
+        except rospy.ServiceException as e:
+            rospy.logerr(f"Service call failed: {e}")
